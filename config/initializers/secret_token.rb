ef08881b5a39e6b401b2d903bb3ea13c712aa0a2
Andrey Zhozhin <andrey@raitgroup.ru>
2010-12-05 18:36:25 +0500

    Created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrimMars::Application.config.secret_token = '3e54d4984b48218243f0887b20223029da7af1a16e4c8d1eb79e84e2e49ac6c736a8e18a96887dab145e9c626c457905ba02a9c3b3de0e11f4a0ca6f168bc181'
